1bf4289e85b3316c941e19c7cbfeed2873d66474
Ryan Small <ryansmall@Ryans-MacBook-Air.local>
2021-10-27 22:11:04 -0400

    wip: adds jQuery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Constants
-const BASE_URL = ""
-const API_KEY = ""
+const BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FAS&"
+const API_KEY = "LXIO70U1WOW3YKTB"
 // State Variables
 let apiData;
 // Cached Element References
@@ -8,3 +8,18 @@ const $main = $('main');
 // Event Listeners
 
 // Functions
+function getdata(){
+    // get api data and assign it to to our api data variable
+    $.ajax(BASE_URL + 'apikey=' + API_KEY)
+    .then(function(data) {
+        apiData = data;
+        render();
+    }, function (error) {
+
+    });
+}
+
+function render(){
+    // transfer our api data to the DOM
+    console.log(apiData)
+}
