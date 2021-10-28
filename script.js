96ea7c7779313cd73f88e41ae63d71d0017f10fb
Ryan Small <ryansmall@Ryans-MacBook-Air.local>
2021-10-27 21:33:13 -0400

    wip: adding HTML elements

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('js is loaded')
+// Constants
+const BASE_URL = ""
+const API_KEY = ""
+// State Variables
+let apiData;
+// Cached Element References
+const $main = $('main');
+// Event Listeners
+
+// Functions
