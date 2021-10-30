88d1fccb4edf9288ba2326160c581d92d46ef43e
Ryan <ryan.small@feapc.com>
2021-10-30 14:22:29 -0400

    wip: cleaning JS code

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Constants
-const BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FAS&"
-const API_KEY = "LXIO70U1WOW3YKTB"
+const BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FAS&";
+const API_KEY = "LXIO70U1WOW3YKTB";
 // State Variables
 let apiData;
 // Cached Element References
-const $main = $('main');
+const $main = $("main");
 // Event Listeners
 
 // Functions
 function getdata(){
     // get api data and assign it to to our api data variable
-    $.ajax(BASE_URL + 'apikey=' + API_KEY)
+    $.ajax(BASE_URL + "apikey=" + API_KEY)
     .then(function(data) {
         apiData = data;
         render();
     }, function (error) {
-
+        console.log("error")
     });
 }
 
