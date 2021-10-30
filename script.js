92670597f9cbd50913444c65459597c249b7002a
Ryan Small <ryan.small@feapc.com>
2021-10-30 16:58:16 -0400

    wip: adding HTML elements

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Constants
-const BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FAS&";
+const BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
 const API_KEY = "LXIO70U1WOW3YKTB";
 // State Variables
 let apiData;
 // Cached Element References
 const $main = $("main");
+const $var = "FAS" // need input value to be dynamic
 // Event Listeners
 
 // Functions
-const getdata = function fetchApiData() {
+const getData = function fetchApiData() {
     // fetch the api data and assign it to the apiData variable
-    $.ajax(BASE_URL + "apikey=" + API_KEY)
+    $.ajax(BASE_URL + $var + "&apikey=" + API_KEY)
     .then(function(data) {
         apiData = data;
         render();
