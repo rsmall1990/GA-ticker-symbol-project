fdbff6982954c495724b6bb4363d1203f99e41d3
Ryan Small <ryan.small@feapc.com>
2021-11-03 16:29:59 -0400

    feat: adding comp desc

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 $(function () {
 
 // Constants
-const BASE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
+const BASE_URL = "https://www.alphavantage.co/query?function=";
 const API_KEY = "LXIO70U1WOW3YKTB";
 
 // Variables
-let apiData;
+let quoteData;
+let compData;
 
 // Cached Element References
 const $form = $('form');
@@ -20,12 +21,20 @@ $form.on("submit", submitHandler);
 // Functions
 function submitHandler(evt) {
     evt.preventDefault();
-    const ticker = $input.val()
-    // fetch the api data and assign it to the apiData variable
-    $.ajax(BASE_URL + ticker + "&apikey=" + API_KEY)
+    const ticker = $input.val();
+    // fetch the GLOBAL_QUOTE data and assign it to the quoteData variable
+    $.ajax(BASE_URL + "GLOBAL_QUOTE&symbol=" + ticker + "&apikey=" + API_KEY)
     .then(function(data) {
-        apiData = data;
-        console.log(apiData)
+        quoteData = data;
+        console.log(quoteData)
+        displayDataInDom();
+    }, function (error) {
+        console.log("error")
+    });
+    $.ajax(BASE_URL + "OVERVIEW&symbol=" + ticker + "&apikey=" + API_KEY)
+    .then(function(data) {
+        compData = data;
+        console.log(compData)
         displayDataInDom();
     }, function (error) {
         console.log("error")
@@ -36,10 +45,12 @@ function displayDataInDom() {
     // transfer our api data to the DOM
     // have to use bracket notation becuase of all the spaces in the object names
     $main.html(`
-        <p id="ticker">Data for: ${apiData["Global Quote"]["01. symbol"]}</p>
-        <p id="price">Last Price: ${apiData["Global Quote"]["05. price"]}</p>
-        <p id="volume">Volume: ${apiData["Global Quote"]["06. volume"]}</p>
-        <p id="change">Percent Change: ${apiData["Global Quote"]["10. change percent"]}</p>
+        <p id="ticker"><b>Ticker Symbol:</b> ${quoteData["Global Quote"]["01. symbol"]}</p>
+        <p id="name"><b>Company Description:</b> ${compData["Name"]}</p>
+        <p id="price"><b>Last Price:</b> ${quoteData["Global Quote"]["05. price"]}</p>
+        <p id="volume"><b>Volume:</b> ${quoteData["Global Quote"]["06. volume"]}</p>
+        <p id="change"><b>Percent Change:</b> ${quoteData["Global Quote"]["10. change percent"]}</p>
+        <p id="description"><b>Company Description:</b> ${compData["Description"]}</p>
         `
         );
     $input.val("");
